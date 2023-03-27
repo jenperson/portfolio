2c6229e684e81043b95bfeca169dc087051f9e19
Jen Person <personjeh@gmail.com>
2023-03-27 15:13:53 -0500

    updating css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import _ from 'lodash';
 import './style.css';
 
+const bio = document.getElementById('bio');
+
+if (bio) {
+  bio.innerHTML = "Jen is a Senior Developer Relations Engineer at Google focusing on machine learning. </br> She believes that machine learning should be accessible to everyone, and hopes to make that possible through her advocacy. Jen is currently co-starring with JavaScript in a buddy cop comedy where the two don't see eye to eye but are forced to work together, eventually forming a solid loving bond through a series of hilarious misadventures."
+}
+
